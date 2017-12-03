9c991b3f2f390bbd235e5cf420157a37d5f4ee53
BFAlex <838318741@qq.com>
2017-12-04 06:24:28 +0800

    Initial commit

---
+++ app/src/main/java/com/bf/func/BFLoginActivity.java
@@ -0,0 +1,20 @@
+package com.bf.func;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.bf.a1.bfandroidpractice.R;
+
+/**
+ * Created by 1 on 2017/12/2.
+ */
+
+public class BFLoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bflogin);
+    }
+}
